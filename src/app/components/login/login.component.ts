b205c6178a15a02d97c5d3ee53753d37f2e23f26
gabriel-g4 <gbgarcia642@gmail.com>
2025-06-04 20:13:57 -0300

    feat: login html

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  mensaje: string = "";
+
+  constructor(private router: Router) {}
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  async onSubmit(){
+
+    if (this.loginForm.valid){
+      try {
+
+      } catch (error) {
+        
+      }
+    }
+  }
 }
