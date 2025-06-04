9454479247f9700815ca9b56f191c8789c43bd3c
gabriel-g4 <gbgarcia642@gmail.com>
2025-06-04 19:44:05 -0300

    feat: componentes y navegacion, nav y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: "login", 
+        loadComponent: ()=> import('./components/login/login.component').then(c => c.LoginComponent), 
+    },
+    {
+        path: "mi-perfil", 
+        loadComponent: ()=> import('./components/mi-perfil/mi-perfil.component').then(c => c.MiPerfilComponent), 
+    },
+    {
+        path: "publicaciones", 
+        loadComponent: ()=> import('./components/publicaciones/publicaciones.component').then(c => c.PublicacionesComponent), 
+    },
+    {
+        path: "registro", 
+        loadComponent: ()=> import('./components/registro/registro.component').then(c => c.RegistroComponent), 
+    },
+    { 
+        path: "**", 
+        redirectTo: "/publicaciones", 
+        pathMatch: "full"
+    },
+];
